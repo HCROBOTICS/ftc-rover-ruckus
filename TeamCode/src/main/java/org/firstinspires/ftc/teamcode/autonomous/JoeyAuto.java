0dea1314ed72f4137b40821e3b082fcceb9da5fc
JChris-HCROBO <johnachristianson@gmail.com>
2019-02-13 18:22:32 -0600

    created class JoeyAuto for joey to play around with and maybe do something useful

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/JoeyAuto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.NateHardware;
+
+public abstract class JoeyAuto extends LinearOpMode {
+    enum Task {}
+
+    private NateHardware robot = new NateHardware();
+
+    @Override
+    public void runOpMode() {
+        // Nothing to see here.
+    }
+}
